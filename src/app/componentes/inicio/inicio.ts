c7c612906cc3cd8d8970a3666973e97e5a65e240
Edgardo Cayo <edgardo.cayo.miranda@cftmail.cl>
2025-10-09 18:18:08 -0300

    subida con formulario reactivo sin bosotrap

--- src/app/componentes/inicio/inicio.ts
+++ src/app/componentes/inicio/inicio.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-inicio',
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './inicio.html',
   styleUrl: './inicio.css'
 })
 export class Inicio {
+  //Definimos el formulario reactivo
+  usuarioForm = new FormGroup({
+    nombre: new FormControl('', Validators.required),
+    correo: new FormControl('', [Validators.required, Validators.email])
+  });
 
+
+  enviar() {
+    if (this.usuarioForm.valid) {
+      console.log('Datos enviados:', this.usuarioForm.value);
+      alert('Formulario enviado con éxito');
+    } else {
+      alert('Por favor, completa todos los campos correctamente');
+    }
+  }
 
 }
 
