5561dfcc01cbc080926e3a9d104c1f2d60103bdf
Edgardo Cayo <edgardo.cayo.miranda@cftmail.cl>
2025-10-09 18:14:51 -0300

    subida de proyecto esquelo con formulario inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Inicio } from './componentes/inicio/inicio';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component: Inicio
+    }
+];
---
+++ src/app/componentes/inicio/inicio.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.html',
+  styleUrl: './inicio.css'
+})
+export class Inicio {
+
+
+}
+
+
